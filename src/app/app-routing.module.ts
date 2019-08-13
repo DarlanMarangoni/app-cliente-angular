29d3c8441209b6ce87396e8184775b4836a0e12b
Silva <darlan.ricardo.silva@accenture.com>
2019-08-13 14:35:51 -0300

    Direcionando paginas não localizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClienteListComponent } from './clientes/cliente-list/cliente-list.component';
 import { ClienteFormComponent } from './clientes/cliente-form/cliente-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: 'clientes', component: ClienteListComponent },
-  { path: 'clientes/id', component: ClienteFormComponent }
+  { path: 'clientes/id', component: ClienteFormComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http'
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientesModule } from './clientes/clientes.module';
+import { ErrorsModule } from './errors/errors.module';
 
 
 @NgModule({
@@ -16,7 +16,8 @@ import { ClientesModule } from './clientes/clientes.module';
     BrowserModule,
     AppRoutingModule,
     ClientesModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
