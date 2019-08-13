a76c4c5b4bd3b50b39fb3f0da035b2b5c301e8ec
Silva <darlan.ricardo.silva@accenture.com>
2019-08-13 11:39:54 -0300

    Criando o primeiro component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cliente/cliente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ap-cliente',
+    templateUrl: './cliente.component.html'
+})
+export class ClienteComponent{
+
+    id='1'
+    nome='darlan';
+    cpf='123123123';
+    dataNascimento='30-05-1990';
+}
